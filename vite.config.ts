b8830c4f2d8f5c3f8d417312ebffa2651faf1a2a
kamlesh <htkamleshchoudhary@gmail.com>
2026-09-07 20:42:43 +0530

    fix vercel asset paths

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: '/sih-ner-platform/',
+  base: '/',
   plugins: [
     react(),
     VitePWA({
